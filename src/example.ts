0148aca9ae9e7cc48c3e8489095a40843c0f6797
marcustyphoon <marcustyphoon@gmail.com>
2026-07-09 15:17:32 -0700

    Update example.ts

--- src/example.ts
+++ src/example.ts
@@ -1,3 +1,23 @@
 #!/usr/bin/env bun
 
+import { parseArgs } from 'util';
+
+const { values, positionals } = parseArgs({
+  args: Bun.argv,
+  options: {
+    input: { type: 'string', short: 'i' },
+    force: { type: 'boolean', short: 'f' },
+  },
+  strict: true,
+  allowPositionals: true,
+});
+
+console.log('args:', { values, positionals });
+
 console.log(await Bun.$`git status`.text());
+
+const packageJson = (await Bun.file('package.json').json()) as {
+  devDependencies: Record<string, string>;
+};
+
+console.log('current dev dependencies are:', Object.keys(packageJson.devDependencies).join(', '));
